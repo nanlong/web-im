2209a27650eb8d209e2aa9ef2d6553f30eeb29bb
Jeff <200006506@qq.com>
2016-11-04 17:23:24 +0800

    Update config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :zheye, Zheye.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "zheye.im", port: 80],
+  check_origin: false,
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
--- web/static/js/web-chat-socket.js
+++ web/static/js/web-chat-socket.js
@@ -1,6 +1,8 @@
 import {Socket, Presence} from "phoenix"
 
-let socket = new Socket("/socket", {params: {
+let socket_url = document.domain == "localhost" ? "/socket" : "http://www.zheye.im/socket"
+
+let socket = new Socket(socket_url, {params: {
   domain: document.domain == "localhost" ? "www.zheye.im" : document.domain,
   webchat_config: zheye.config.webchat,
 }})
